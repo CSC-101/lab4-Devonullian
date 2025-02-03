62a57feab0250367bdaee877b2e4ac83d4168c92
Devonullian <ulliandevon@gmail.com>
2025-02-02 17:55:15 -0800

    Solve first task

--- lab4.py
+++ lab4.py
@@ -1,9 +1,13 @@
 import data
 
 # Write your functions for each part in the space below.
-
 # Part 1
-
+def first_element(x: list[list[int]]) -> list[int]:
+    ls: list[int] = []
+    for child_list in ls:
+        if child_list is not None or child_list.size() >= 1:
+            ls.append(child_list[0])
+    return ls
 
 # Part 2
 
